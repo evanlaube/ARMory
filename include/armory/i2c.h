0729cf3b370cea86cf8146ac0c8e2d3de1398d04
Evan Laube <laubeevan@gmail.com>
2025-04-27 12:22:21 -0500

    [comment] Add Doxygen style comments on function prototypes

--- include/armory/i2c.h
+++ include/armory/i2c.h
@@ -73,23 +73,182 @@ typedef enum {
     I2C_ERROR
 } I2CResult;
 
+/**
+ * @brief Gets the I2CMap for the given I2C instance.
+ * 
+ * @param i2c Pointer to the I2C instance to get the mapping for.
+ * @return Pointer to the I2CMap for given I2C instance.
+ */
 const I2CMap *getI2CMap(I2C_TypeDef *i2c);
 
+/**
+ * @brief Initializes the I2C peripheral.
+ * 
+ * Enables the I2C clock, configures the GPIO pins for SCL and SDA,
+ * and sets the I2C clock speed.
+ *
+ * @param i2c Pointer to the I2C instance to initialize.
+ */
 void i2cInit(I2C_TypeDef *i2c);
+
+/**
+ * @brief Starts the I2C communication.
+ *
+ * Issues a START condition on the I2C bus.
+ *
+ * @param i2c Pointer to the I2C instance to start communication on.
+ */
 void i2cStart(I2C_TypeDef *i2c);
+
+/**
+ * @brief Stops the I2C communication.
+ *
+ * Issues a STOP condition on the I2C bus.
+ *
+ * @param i2c Pointer to the I2C instance to stop communication on.
+ */
 void i2cStop(I2C_TypeDef *i2c);
 
+/**
+ * @brief Sends the device address on the I2C bus.
+ *
+ * Sends the device address shifted left by 1 bit, setting the LSB to the 
+ * read bit. If read is true, the LSB is set to 1, indicating a read operation.
+ *
+ * @param i2c Pointer to the I2C instance to send the address on.
+ * @param addr The device address to send.
+ * @param read True if following with a read operation, false for write.
+ *
+ * @returns I2CResult indicating the result of the operation.
+ */
 I2CResult i2cSendAddr(I2C_TypeDef *i2c, uint8_t addr, bool read);
+
+/**
+ * @brief Sends a single data byte on the I2C bus.
+ *
+ * Sends a single byte of data on the specified I2C bus. The byte has no device
+ * addressing or register addressing, it is simply a raw byte.
+ *
+ * @param i2c Pointer to the I2C instance to send the data on.
+ * @param data The data byte to send.
+ *
+ * @returns I2CResult indicating the result of the operation.
+ *
+ * @note This function is intended to only be called inside of other I2C functions,
+ *       but is exposed for convenience.
+ */
 I2CResult i2cSendData(I2C_TypeDef *i2c, uint8_t data);
 
+/**
+ * @brief Writes a byte to a specific register of a device on the I2C bus.
+ *
+ * Sends the device address byte, followed by the register address byte, 
+ * followed by a single byte of data to the specified I2C bus. This is a 
+ * complete I2C transaction.
+ *
+ * @param i2c Pointer to the I2C instance to send the data on.
+ * @param devAddr The device address to send the data to.
+ * @param regAddr The register address to write to.
+ * @param data The data byte toe send.
+ *
+ * @returns I2CResult indicating the result of the operation.
+ */
 I2CResult i2cWriteByte(I2C_TypeDef *i2c, uint8_t devAddr, uint8_t regAddr, uint8_t data);
+
+/**
+ * @brief Writes a raw byte of data to a device on the I2C bus.
+ *
+ * Sends the device address byte, followed by a single byte of data to the
+ * specified I2C bus.
+ *
+ * @param i2c Pointer to the I2C instance to send the data on.
+ * @param devAddr The device address to send the data to.
+ * @param data The data byte to send.
+ *
+ * @returns I2CResult indicating the result of the operation.
+ */
 I2CResult i2cWriteRaw(I2C_TypeDef *i2c, uint8_t devAddr, uint8_t data);
+
+/**
+ * @brief Writes multiple bytes of data to a device on the I2C bus.
+ *
+ * Sends the address byte, followed by the bytes in the data array to the
+ * specified I2C bus.
+ *
+ * @param i2c Pointer to the I2C instance to send the data on.
+ * @param devAddr The device address to send the data bytes to.
+ * @param data Pointer to the array of data bytes to send.
+ * @param n The number of bytes in the data array
+ *
+ * @returns I2CResult indicating the result of the operation.
+ *
+ * @note This function does not send a register address. If needed, it should
+ *       be the first byte of the data array.
+ */
 I2CResult i2cWriteBytes(I2C_TypeDef *i2c, uint8_t devAddr, uint8_t *data, uint16_t n);
 
+/**
+ * @brief Reads a byte from a specific register of a device on the I2C bus.
+ *
+ * Sends the device address byte, followed by the register address byte, then
+ * issues a repeated start condition, followed by sending the device address 
+ * byte again with the read bit set. Then receives the data byte from the device.
+ * The data byte is stored in the passed data pointer.
+ *
+ * @param i2c Pointer to the I2C instance to read the data from.
+ * @param devAddr The device address to read the data from.
+ * @param regAddr The register address to read from.
+ * @param data Pointer to the variable to store the received data byte in.
+ *
+ * @return I2cResult indicating the result of the operation.
+ */
 I2CResult i2cReadByte(I2C_TypeDef *i2c, uint8_t devAddr, uint8_t regAddr, uint8_t *data);
+
+/**
+ * @brief Reads a raw byte of data from a device on the I2C bus.
+ *
+ * Sends the device address byte with the read bit set, the receives the data
+ * byte from the device. The data byte is stored in the passed data pointer.
+ *
+ * @param i2c Pointer to the I2C instance to read the data from.
+ * @param devAddr The device address to read the data from.
+ * @param data Pointer to the variable to store the reveived data byte it.
+ */
 I2CResult i2cReadRaw(I2C_TypeDef *i2c, uint8_t devAddr, uint8_t *data);
-I2CResult i2cReadBytes(I2C_TypeDef *i2c, uint8_t devAddr, uint8_t regAddr, uint8_t *buffer, uint16_t len);
-I2CResult i2cReadRawBytes(I2C_TypeDef *i2c, uint8_t devAddr, uint8_t *buffer, uint16_t len);
+
+/**
+ * @brief Reads multiple bytes of data from a device on the I2C bus.
+ *
+ * Sends the device address byte, followed by the register address byte, then
+ * issues a repeated start condition, followed by sending the device address 
+ * byte again with the read bit set. Then, receives the data bytes from the
+ * device.
+ *
+ * @param i2c Pointer to the I2C instance to read the data from.
+ * @param devAddr The device address to read the data from.
+ * @param regAddr The register address to read from
+ * @param buffer Pointer to the buffer array to store the received data bytes in.
+ * @param len The number of bytes to read.
+ *
+ * @return I2CResult indicating the result of the operation.
+ */
+I2CResult i2cReadBytes(I2C_TypeDef *i2c, uint8_t devAddr, uint8_t regAddr, 
+        uint8_t *buffer, uint16_t len);
+
+/**
+ * @brief Reads multiple bytes of raw data from a device on the I2C bus.
+ *
+ * Sendds the device address byte with the read bit set, then receives the 
+ * specified number of data bytes from the device and stores the at the passed 
+ * data pointer.
+ *
+ * @param i2c Pointer to the I2C instance to read the data from.
+ * @param devAddr The device address to read the data from.
+ * @param buffer Pointer to the buffer array to store the received data bytes in.
+ * @param len The number of bytes to read.
+ */
+I2CResult i2cReadRawBytes(I2C_TypeDef *i2c, uint8_t devAddr, uint8_t *buffer, 
+        uint16_t len);
 
 
 #endif // !I2C_H
